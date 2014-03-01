b905e37664df6074c60bf86cb61ae6b22849e608
Berlizov <berlizov@me.com>
2014-03-01 14:06:46 +0200

    fixes

--- src/CalcField.java
+++ src/CalcField.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class CalcField extends JTextField{
+public class CalcField extends JTextField {
 
     boolean memory;
     String additionalString;
@@ -14,39 +14,70 @@ public class CalcField extends JTextField{
         setDisabledTextColor(Color.BLACK);
         setEnabled(false);
         memory = false;
-        additionalString="";
+        additionalString = "";
         setFont(getFont());
     }
-    public void appendMainText(String str){
-        setText(this.getText()+str);
+
+    public void appendMainString(String str) {
+        if (!((this.getText().indexOf(".") > 0) && (str.equals("."))))
+            setText(this.getText() + str);
+    }
+
+    public void appendAdditionalString(String str) {
+
+        setAdditionalString(getAdditionalString() + str);
     }
 
 
     @Override
     public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D)g;
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-        setFont(new Font(getFont().getFontName(), Font.PLAIN, getHeight()-30));
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+        setFont(new Font(getFont().getFontName(), Font.PLAIN, getHeight() - 30));
         super.paint(g);
         g2d.setFont(new Font(getFont().getFontName(), Font.PLAIN, 12));
 
-        g2d.drawString(additionalString, (int)(getWidth()-(g2d.getFontMetrics().stringWidth(additionalString))-4), 12);
-        if(memory) {
+        g2d.drawString(additionalString, (int) (getWidth() - (g2d.getFontMetrics().stringWidth(additionalString)) - 4), 12);
+        if (memory) {
             g2d.setColor(Color.red);
             g2d.drawString("M", 12, getHeight() - 12);
         }
     }
 
+    public void deleteLast() {
+        String str = getText();
+        if (str.length() < 2)
+            clearMainString();
+        else
+            setText(str.substring(0, str.length() - 1));
+    }
 
     public boolean isMemory() {
         return memory;
     }
 
+    public String getMainString() {
+        return getText();
+    }
+
+    public void setMainString(String s) {
+        setText(s);
+    }
+
+    public void clearMainString() {
+        setText("0");
+    }
+
     public void setMemory(boolean memory) {
         this.memory = memory;
     }
+
     public String getAdditionalString() {
         return additionalString;
+    }
+
+    public void clearAdditionalString() {
+        setAdditionalString("");
     }
 
     public void setAdditionalString(String additionalString) {
--- src/UI.java
+++ src/UI.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 public class UI extends JFrame implements ActionListener {
     final CalcField cf;
+    boolean func = false;
+
     public UI() {
         super("Calculator");
         setMinimumSize(new Dimension(300, 300));
@@ -16,55 +18,73 @@ public class UI extends JFrame implements ActionListener {
         c.gridx = 0;
         c.gridy = 0;
         c.gridwidth = 5;
-        cf=new CalcField();
+        cf = new CalcField();
         cf.setMemory(true);
         add(cf, c);
 
-        String[][] str={{"MC","MR","MS","M+","M-"},
-                {"←","CE","C","±","√"},
-                {"7","8","9","/","%"},
-                {"4","5","6","*","1/x"},
-                {"1","2","3","-","="},
-                {"0","",",","+",""}};
-        for(int i=1;i<7;i++)
-            for(int j=0;j<5;j++)
-            {
-                if (!str[i-1][j].equals(""))
-                {
+        String[][] str = {{"MC", "MR", "MS", "M+", "M-"},
+                {"←", "CE", "C", "±", "√"},
+                {"7", "8", "9", "/", "%"},
+                {"4", "5", "6", "*", "1/x"},
+                {"1", "2", "3", "-", "="},
+                {"0", "", ",", "+", ""}};
+        for (int i = 1; i < 7; i++)
+            for (int j = 0; j < 5; j++) {
+                if (!str[i - 1][j].equals("")) {
                     c.gridwidth = 1;
                     c.gridheight = 1;
-                    if(str[i-1][j].equals("0"))
+                    if (str[i - 1][j].equals("0"))
                         c.gridwidth = 2;
-                    if(str[i-1][j].equals("="))
+                    if (str[i - 1][j].equals("="))
                         c.gridheight = 2;
 
 
                     c.gridx = j;
                     c.gridy = i;
-                    JButton b=new JButton(str[i-1][j]);
+                    JButton b = new JButton(str[i - 1][j]);
                     b.addActionListener(this);
                     add(b, c);
                 }
             }
         setVisible(true);
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        JButton button = (JButton)e.getSource();
-        if((button.getText().charAt(0)>='0')&&(button.getText().charAt(0)<='9'))
-        {
-            if(cf.getText().equals("0"))
-                cf.setText(button.getText());
+        JButton button = (JButton) e.getSource();
+        if ((button.getText().charAt(0) >= '0') && (button.getText().charAt(0) <= '9')) {
+            if (func) {
+                cf.clearMainString();
+                func = false;
+            }
+            if (cf.getText().equals("0"))
+                cf.setMainString(button.getText());
             else
-                cf.appendMainText(button.getText());
-        }
-        else if(button.getText().equals("."))
-            cf.appendMainText(button.getText());
-        else if(button.getText().equals("CE"))
-            cf.setText("");
-        else if(button.getText().equals("C"))
-        {
-            cf.setText("");//todo
+                cf.appendMainString(button.getText());
+        }else if (button.getText().equals(","))
+            cf.appendMainString(".");
+        else if (button.getText().equals("←")) {
+            cf.deleteLast();
+        }else if (button.getText().equals("CE"))
+            cf.clearMainString();
+        else if (button.getText().equals("C")) {
+            cf.clearMainString();
+            cf.clearAdditionalString();
+        }else if (button.getText().equals("±")) {
+            String str = cf.getMainString();
+            if (str.charAt(0) == '-')
+                cf.setMainString(str.substring(1, str.length()));
+            else
+                cf.setMainString("-" + str);
+            func = false;
+        }else if (button.getText().equals("=")) {
+            func = true;
+            cf.appendAdditionalString(cf.getMainString());
+            cf.setMainString(Calculator.calculat(cf.getAdditionalString()));
+            cf.clearAdditionalString();
+        }else {
+            func = true;
+            cf.appendAdditionalString(cf.getMainString() + button.getText());
         }
     }
 }
